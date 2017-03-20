f4238a06f90dd63b377d380b902df71856e19e00
Eric C. Weig <eweig@email.uky.edu>
2017-03-19 20:54:31 -0400

    Add files via upload

--- views/admin/index/browse.php
+++ views/admin/index/browse.php
@@ -1,18 +1,18 @@
 <?php 
-    echo head(array('title' => __('CSV Import')));
+    echo head(array('title' => __('OHMS Import')));
 ?>
-<?php echo common('csvimport-nav'); ?>
+<?php echo common('ohmsimport-nav'); ?>
 <div id="primary">
     <h2><?php echo __('Status'); ?></h2>
     <?php echo flash(); ?>
     <div class="pagination"><?php echo pagination_links(); ?></div>
-    <?php if (iterator_count(loop('CsvImport_Import'))): ?>
+    <?php if (iterator_count(loop('OhmsImport_Import'))): ?>
     <table class="simple" cellspacing="0" cellpadding="0">
         <thead>
             <tr>                
                 <?php
                 $browseHeadings[__('Import Date')] = 'added';
-                $browseHeadings[__('CSV File')] = 'original_filename';
+                $browseHeadings[__('OHMS File')] = 'original_filename';
                 $browseHeadings[__('Imported Items')] = null;
                 $browseHeadings[__('Skipped Items')] = 'skipped_item_count';
                 $browseHeadings[__('Skipped Rows')] = 'skipped_row_count';
@@ -24,41 +24,41 @@
         </thead>
         <tbody>            
             <?php $key = 0; ?>
-            <?php foreach (loop('CsvImport_Import') as $csvImport): ?>
+            <?php foreach (loop('OhmsImport_Import') as $ohmsImport): ?>
             <tr class="<?php if(++$key%2==1) echo 'odd'; else echo 'even'; ?>">
             
-                <td><?php echo html_escape(format_date($csvImport->added, Zend_Date::DATETIME_SHORT)); ?></td>
-                <td><?php echo html_escape($csvImport->original_filename); ?></td>
+                <td><?php echo html_escape(format_date($ohmsImport->added, Zend_Date::DATETIME_SHORT)); ?></td>
+                <td><?php echo html_escape($ohmsImport->original_filename); ?></td>
                 
-                <?php $importedItemCount = $csvImport->getImportedItemCount(); ?>
+                <?php $importedItemCount = $ohmsImport->getImportedItemCount(); ?>
                 <td><?php echo html_escape($importedItemCount); ?></td>
-                <td><?php echo html_escape($csvImport->skipped_item_count); ?></td>
-                <td><?php echo html_escape($csvImport->skipped_row_count); ?></td>
+                <td><?php echo html_escape($ohmsImport->skipped_item_count); ?></td>
+                <td><?php echo html_escape($ohmsImport->skipped_row_count); ?></td>
                 
-                <td><?php echo html_escape(__(Inflector::humanize($csvImport->status, 'all'))); ?></td>
+                <td><?php echo html_escape(__(Inflector::humanize($ohmsImport->status, 'all'))); ?></td>
                 <?php
-                    if ($csvImport->isCompleted() 
-                        || $csvImport->isStopped()
-                        || ($csvImport->isImportError() && $importedItemCount > 0)): ?>
+                    if ($ohmsImport->isCompleted() 
+                        || $ohmsImport->isStopped()
+                        || ($ohmsImport->isImportError() && $importedItemCount > 0)): ?>
                     <?php 
                     $undoImportUrl = $this->url(array('action' => 'undo-import',
-                                                      'id' => $csvImport->id),
+                                                      'id' => $ohmsImport->id),
                                                       'default'); 
                     ?>
                     <td>
-                        <a href="<?php echo html_escape($undoImportUrl);  ?>" class="csv-undo-import delete-button"><?php echo html_escape(__('Undo Import')); ?></a>
+                        <a href="<?php echo html_escape($undoImportUrl);  ?>" class="ohms-undo-import delete-button"><?php echo html_escape(__('Undo Import')); ?></a>
                     </td>
-                <?php elseif ($csvImport->isUndone() || 
-                              $csvImport->isUndoImportError() || 
-                              $csvImport->isOtherError() || 
-                              ($csvImport->isImportError() && $importedItemCount == 0)): ?>
+                <?php elseif ($ohmsImport->isUndone() || 
+                              $ohmsImport->isUndoImportError() || 
+                              $ohmsImport->isOtherError() || 
+                              ($ohmsImport->isImportError() && $importedItemCount == 0)): ?>
                     <?php 
                     $clearHistoryImportUrl = $this->url(array('action' => 'clear-history',
-                                                              'id' => $csvImport->id),
+                                                              'id' => $ohmsImport->id),
                                                               'default'); 
                     ?>
                     <td>
-                        <a href="<?php echo html_escape($clearHistoryImportUrl);  ?>" class="csv-clear-history delete-button"><?php echo html_escape(__('Clear History')); ?></a>
+                        <a href="<?php echo html_escape($clearHistoryImportUrl);  ?>" class="ohms-clear-history delete-button"><?php echo html_escape(__('Clear History')); ?></a>
                     </td>
                 <?php else: ?>
                     <td></td>
@@ -75,7 +75,7 @@
 <script type="text/javascript">
 //<![CDATA[
 jQuery(document).ready(function () {
-    Omeka.CsvImport.confirm();
+    Omeka.OhmsImport.confirm();
 });
 //]]>
 </script>
