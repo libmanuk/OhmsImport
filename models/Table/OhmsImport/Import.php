3c051d6b96f931144475651e02bc5dcb22058b98
Eric C. Weig <eweig@email.uky.edu>
2017-09-13 20:34:37 -0400

    Create Import.php

---
+++ models/Table/OhmsImport/Import.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Table_OhmsImport_Import class
+ * 
+ * @copyright Copyright 2007-2012 Roy Rosenzweig Center for History and New Media
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt GNU GPLv3
+ * @package OhmsImport
+ */
+class Table_OhmsImport_Import extends Omeka_Db_Table
+{
+}
